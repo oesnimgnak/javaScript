fea3c4a829ce5bcd37f7bc50d9792ac2da8dc65c
Min <106712982+oesnimgnak@users.noreply.github.com>
2023-09-22 12:19:36 +0900

    23.09.20
    23.09.20

--- chapter4/js/scope-3.js
+++
@@ -1,8 +0,0 @@
-var hi = "hello";       //전역 변수
-
-function greeting() {
-    hi = "bye"; //데이터 재정의 . 함수안에서 전역 변수 수정 가능
-}
-console.log(hi);    //hello
-greeting();     //재정의 함수 불러오기
-console.log(hi);    //bye
--- chapter4/js/scope-4.js
+++
@@ -1,8 +0,0 @@
-var hi = "hello";       //전역 변수
-
-function greeting() {
-   var hi = "bye"; //지역 변수
-}
-console.log(hi);    //hello
-greeting();     //재정의 함수 불러오기
-console.log(hi);    //hello
